10db1c64744565dd85379ba44e34ea173b9b6851
Craig Taylor <craig.taylor3@engineering.digital.dwp.gov.uk>
2022-11-21 12:23:04 +0000

    attempt to get gov uk styles in

--- stories/button/Button.tsx
+++ stories/button/Button.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './button.css';
 
 interface ButtonProps {
   /**
@@ -36,13 +35,8 @@ export const Button = ({
 }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
+    <button className="govuk-button" data-module="govuk-button">
+      Save and continue
     </button>
   );
 };
