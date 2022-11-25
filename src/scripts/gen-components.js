a8deb3077439c3a3dd04e069cdadc622d0f87b7d
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2022-11-25 23:31:32 +0000

    gen component index

--- src/scripts/gen-components.js
+++ src/scripts/gen-components.js
@@ -14,13 +14,21 @@ components.forEach(component => {
   }
 });
 
-// create folders for all compoents we dont have a folder for in this project
+// create components we dont have in this project
 componentsToGen.forEach(c => {
   const camelisedComponent = c.replace(/-./g, x=>x[1].toUpperCase());
   const casedComponent = camelisedComponent[0].toUpperCase() + camelisedComponent.substring(1)
   let componentPath = path.resolve(__dirname, `../components/${casedComponent}`);
+  // folder
   if (!fs.existsSync(componentPath)) {
     console.log(`... making directory for ${c}`);
     fs.mkdirSync(componentPath);
+  }
+  // index file
+  const indexFilePath = path.resolve(componentPath, 'index.ts');
+  if (!fs.existsSync(indexFilePath)) {
+    console.log(`... writing index file for ${casedComponent} component`);
+    const indexFileContent = `export { default } from './${casedComponent}';`
+    fs.writeFileSync(indexFilePath, indexFileContent);
   }
 });
