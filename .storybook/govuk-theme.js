facc80e2e124a5e732ef6521fd6f6db341222449
Nathan Maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2022-12-15 10:16:44 +0000

    Update build script to include static assets

--- .storybook/govuk-theme.js
+++ .storybook/govuk-theme.js
@@ -2,7 +2,7 @@ import { create } from '@storybook/theming';
 
 export default create({
   base: 'light',
-  brandTitle: 'GOVUK Frontend React',
+  brandTitle: 'GOV.UK UI',
   brandUrl: 'https://design-system.service.gov.uk/',
   brandImage: './src/assets/govuk-ui-logo-light-mode.png',
   brandTarget: '_blank',
