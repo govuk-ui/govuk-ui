2c5c2e91e7035db0cb3d988e89c68ffd49001a77
Nathan Maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2022-12-15 10:21:14 +0000

    Updated ErrorMessage story prope

--- src/stories/ErrorMessage.stories.tsx
+++ src/stories/ErrorMessage.stories.tsx
@@ -22,9 +22,8 @@ fixtures.fixtures.forEach((fixture) => {
   let story: Story = stories.find((s) => s.name === fixture.name.replace(/[^a-z0-9s]/gi, "")) || {};
   if (story.name === fixture.name) {
     story.args = {
-      text: fixture.options.text,
+      children: fixture.options.text,
       visuallyHiddenText: fixture.options.visuallyHiddenText,
-      html: fixture.options.html,
       id: fixture.options.id,
       classes: fixture.options.classes,
       attributes: fixture.options.attributes,
