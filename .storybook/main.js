e684290d7fdaa879d9434d09beb54e2f2a985311
Nathan Maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2023-02-22 22:25:13 +0000

    Started writing Radios documentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,9 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    '../src/**/*.mdx',
+  ],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   "framework": {
     name: "@storybook/react-vite",
@@ -13,5 +17,5 @@ module.exports = {
   },
   docsPage: {
     docs: "automatic"
-  }
+  },
 };
--- src/stories/components/Radios.stories.tsx
+++ src/stories/components/Radios.stories.tsx
@@ -6,6 +6,9 @@ import fixtures from "govuk-frontend/govuk/components/radios/fixtures.json";
 import RadioItem from "../../components/RadioItem";
 import Label from "../../components/Label";
 import Hint from "../../components/Hint";
+import Legend from "../../layout/Legend";
+import Typography from "../../typography/Typography";
+import Input from "../../components/Input";
 
 const meta: Meta<typeof Radios> = {
   title: "Radios",
@@ -15,115 +18,154 @@ const meta: Meta<typeof Radios> = {
 export default meta;
 type Story = StoryObj<typeof Radios>;
 
-const primary: Story = { name: "default" };
-const inline: Story = { name: "inline" };
-const withDisabled: Story = { name: "with disabled" };
-const withLegendAsPageHeading: Story = { name: "with legend as page heading" };
-const withAMediumLegend: Story = { name: "with a medium legend" };
-const withADivider: Story = { name: "with a divider" };
-const withHintsOnItems: Story = { name: "with hints on items" };
-const withoutFieldset: Story = { name: "without fieldset" };
-const withFieldsetAndErrorMessage: Story = { name: "with fieldset and error message" };
-const withVeryLongOptionText: Story = { name: "with very long option text" };
-const withConditionalItems: Story = { name: "with conditional items" };
-const withConditionalItemsWithSpecialCharacters: Story = { name: "with conditional items with special characters" };
-const withConditionalItemChecked: Story = { name: "with conditional item checked" };
-const prechecked: Story = { name: "prechecked" };
-const precheckedUsingValue: Story = { name: "prechecked using value" };
-const withConditionalItemsAndPrecheckedValue: Story = { name: "with conditional items and prechecked value" };
-const withOptionalFormgroupClassesShowingGroupError: Story = {
-  name: "with optional formgroup classes showing group error",
-};
-const small: Story = { name: "small" };
-const smallWithLongText: Story = { name: "small with long text" };
-const smallWithError: Story = { name: "small with error" };
-const smallWithHint: Story = { name: "small with hint" };
-const smallWithDisabled: Story = { name: "small with disabled" };
-const smallWithConditionalReveal: Story = { name: "small with conditional reveal" };
-const smallInline: Story = { name: "small inline" };
-const smallInlineExtreme: Story = { name: "small inline extreme" };
-const smallWithADivider: Story = { name: "small with a divider" };
-const withIdPrefix: Story = { name: "with idPrefix" };
-
-const stories: Story[] = [];
-stories.push(primary);
-stories.push(inline);
-stories.push(withDisabled);
-stories.push(withLegendAsPageHeading);
-stories.push(withAMediumLegend);
-stories.push(withADivider);
-stories.push(withHintsOnItems);
-stories.push(withoutFieldset);
-stories.push(withFieldsetAndErrorMessage);
-stories.push(withVeryLongOptionText);
-stories.push(withConditionalItems);
-stories.push(withConditionalItemsWithSpecialCharacters);
-stories.push(withConditionalItemChecked);
-stories.push(prechecked);
-stories.push(precheckedUsingValue);
-stories.push(withConditionalItemsAndPrecheckedValue);
-stories.push(withOptionalFormgroupClassesShowingGroupError);
-stories.push(small);
-stories.push(smallWithLongText);
-stories.push(smallWithError);
-stories.push(smallWithHint);
-stories.push(smallWithDisabled);
-stories.push(smallWithConditionalReveal);
-stories.push(smallInline);
-stories.push(smallInlineExtreme);
-stories.push(smallWithADivider);
-stories.push(withIdPrefix);
-
-fixtures.fixtures.forEach((fixture) => {
-  let story: Story = stories.find((s) => s.name === fixture.name.replace(/[^a-z0-9s]/gi, "")) || {};
-  if (story.name === fixture.name) {
-    story.args = {
-      name: fixture.options.name,
-      children: fixture.options.items.map((i:any) => {
-        return (
-          <RadioItem name={fixture.options.name} value={i.value}>
-            <Label>{i.text}</Label>
-            {
-              fixture.options.hint?.text && (
-                <Hint>{fixture.options.hint.text}</Hint>
-              )
-            }
-          </RadioItem>
-        )
-      }),
-      idPrefix: fixture.options.idPrefix,
-      classes: fixture.options.classes,
-      attributes: fixture.options.attributes,
-    };
+const withHeading: Story = {
+  name: 'Where do you live',
+  args: {
+    name: 'whereDoYouLive',
+    children: [
+      <Legend>
+        <Typography variant='l' component='h1'>Where do you live?</Typography>
+      </Legend>,
+      <RadioItem value="england">
+        <Label>England</Label>
+      </RadioItem>,
+      <RadioItem value="scotland">
+        <Label>Scotland</Label>
+      </RadioItem>,
+      <RadioItem value="wales">
+        <Label>Wales</Label>
+      </RadioItem>,
+      <RadioItem value="northernIreland">
+        <Label>Northern Ireland</Label>
+      </RadioItem>
+    ]
   }
-});
+}
+
+const withoutHeading: Story = {
+  name: 'Where do you live without heading',
+  args: {
+    name: 'whereDoYouLive',
+    children: [
+      <Legend>
+        Where do you live?
+      </Legend>,
+      <RadioItem value="england">
+        <Label>England</Label>
+      </RadioItem>,
+      <RadioItem value="scotland">
+        <Label>Scotland</Label>
+      </RadioItem>,
+      <RadioItem value="wales">
+        <Label>Wales</Label>
+      </RadioItem>,
+      <RadioItem value="northernIreland">
+        <Label>Northern Ireland</Label>
+      </RadioItem>
+    ]
+  }
+}
+
+const inline: Story = {
+  name: 'Inline radios',
+  args: {
+    name: 'changedName',
+    classes: 'govuk-radios--inline',
+    children: [
+      <Legend>
+        <Typography variant='l' component='h1'>Have you changed your name?</Typography>
+        <Hint>This includes changing your last name or spelling your name differently.</Hint>
+      </Legend>,
+      <RadioItem value="yes">
+        <Label>Yes</Label>
+      </RadioItem>,
+      <RadioItem value="no">
+        <Label>No</Label>
+      </RadioItem>,
+    ]
+  }
+}
+
+const withHint: Story = {
+  name: 'Radio items with hint',
+  args: {
+    name: 'signInMethod',
+    children: [
+      <Legend>
+        <Typography variant='l' component='h1'>How do you want to sign in?</Typography>
+        <Hint>You’ll need an account to prove your identity and complete your Self Assessment.</Hint>
+      </Legend>,
+      <RadioItem value="governmentGateway">
+        <Label>Sign in with Government Gateway</Label>
+        <Hint>You’ll have a user ID if you’ve registered for Self Assessment or filed a tax return online before.</Hint>
+      </RadioItem>,
+      <RadioItem value="verify">
+        <Label>Sign in with GOV.UK Verify</Label>
+        <Hint>You’ll have an account if you’ve already proved your identity with either Barclays, CitizenSafe, Digidentity, Experian, Post Office, Royal Mail or SecureIdentity.</Hint>
+      </RadioItem>,
+    ]
+  }
+}
+
+const withDivider: Story = {
+  name: 'Radio items with divider',
+  args: {
+    name: 'whereDoYouLive',
+    children: [
+      <Legend>
+        <Typography variant='l' component='h1'>Where do you live?</Typography>
+      </Legend>,
+      <RadioItem value="england">
+        <Label>England</Label>
+      </RadioItem>,
+      <RadioItem value="scotland">
+        <Label>Scotland</Label>
+      </RadioItem>,
+      <RadioItem value="wales">
+        <Label>Wales</Label>
+      </RadioItem>,
+      <RadioItem value="northernIreland">
+        <Label>Northern Ireland</Label>
+      </RadioItem>,
+      <RadioItem divider>or</RadioItem>,
+      <RadioItem value="britishAbroad">
+        <Label>I am a British citizen living abroad</Label>
+      </RadioItem>
+    ]
+  }
+}
+
+const conditional: Story = {
+  name: 'Radio items with conditional',
+  args: {
+    name: 'contactPreference',
+    children: [
+      <Legend>
+        <Typography variant='l' component='h1'>How would you prefer to be contacted?</Typography>
+        <Hint>Select one option.</Hint>
+      </Legend>,
+      <RadioItem value="email" conditional={
+        <Input name="emailAddress" classes="govuk-!-width-one-third">
+          <Label>Email address</Label>
+        </Input>
+      }>
+        <Label>Email</Label>
+      </RadioItem>,
+      <RadioItem value="phone">
+        <Label>Phone</Label>
+      </RadioItem>,
+      <RadioItem value="text">
+        <Label>Text message</Label>
+      </RadioItem>,
+    ]
+  }
+}
 
 export {
-  primary,
+  withHeading,
+  withoutHeading,
   inline,
-  withDisabled,
-  withLegendAsPageHeading,
-  withAMediumLegend,
-  withADivider,
-  withHintsOnItems,
-  withoutFieldset,
-  withFieldsetAndErrorMessage,
-  withVeryLongOptionText,
-  withConditionalItems,
-  withConditionalItemsWithSpecialCharacters,
-  withConditionalItemChecked,
-  prechecked,
-  precheckedUsingValue,
-  withConditionalItemsAndPrecheckedValue,
-  withOptionalFormgroupClassesShowingGroupError,
-  small,
-  smallWithLongText,
-  smallWithError,
-  smallWithHint,
-  smallWithDisabled,
-  smallWithConditionalReveal,
-  smallInline,
-  smallInlineExtreme,
-  smallWithADivider,
-  withIdPrefix,
+  withHint,
+  withDivider,
+  conditional,
 };
