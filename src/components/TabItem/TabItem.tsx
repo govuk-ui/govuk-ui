9a1341c2641c848d88c3846ca3c99f331c726712
Craig Taylor <craig.taylor3@engineering.digital.dwp.gov.uk>
2023-02-23 16:40:16 +0000

    pass id through to tab panel

--- src/components/TabItem/TabItem.tsx
+++ src/components/TabItem/TabItem.tsx
@@ -4,13 +4,14 @@ import TabItemProps from "./TabItem.types";
 export const TabItem = ({
   children,
   label,
+  id,
   classes,
   href,
   selected,
   attributes
 }: TabItemProps) => {
   return (
-    <li className={`govuk-tabs__list-item ${selected && 'govuk-tabs__list-item--selected'}`}>
+    <li className={`govuk-tabs__list-item ${selected && 'govuk-tabs__list-item--selected'}`} id={id} { ...attributes}>
       <a className="govuk-tabs__tab" href={href}>
         { label }
       </a>
