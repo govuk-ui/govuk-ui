6bec04c7d5baab0e1843c2cfa25fadb27c54c8e5
Nathan Maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2023-02-27 13:10:01 +0000

    Updated docs generation script

--- .storybook/govuk-theme.js
+++ .storybook/govuk-theme.js
@@ -1,9 +1,10 @@
 import { create } from '@storybook/theming';
+import image from '../src/assets/govuk-ui-logo-light-mode.png';
 
 export default create({
   base: 'light',
   brandTitle: 'GOV.UK UI',
   brandUrl: 'https://design-system.service.gov.uk/',
-  brandImage: './src/assets/govuk-ui-logo-light-mode.png',
+  brandImage: image,
   brandTarget: '_blank',
 });
--- docs/assets/syntaxhighlighter-3EX6JOZ7.1d83baee.js
+++
@@ -1,2 +0,0 @@
-import{S as n,c as d,s as f}from"./DocsRenderer-QMFTTPNP.62ea0cca.js";import"./iframe.805bd1f6.js";import"../../../../sb-preview/runtime.mjs";import"./index.ef27e5dc.js";import"./_commonjsHelpers.712cc82f.js";import"./_getPrototype.2bb0f921.js";import"./_baseToString.b3297a2e.js";import"./window.2ce53669.js";import"./jsx-runtime.999029ca.js";import"./preview.935d44da.js";export{n as SyntaxHighlighter,d as createCopyToClipboardFunction,f as default};
-//# sourceMappingURL=syntaxhighlighter-3EX6JOZ7.1d83baee.js.map
