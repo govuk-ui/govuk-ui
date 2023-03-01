fb94c75f80b877cbfc59d52d97440fec4555c6ec
Nathan Maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2023-03-01 19:21:45 +0000

    Finalised pagination component to support block level

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,17 +1,18 @@
 import React, {Children, cloneElement, isValidElement} from "react";
 import PaginationProps from "./Pagination.types";
 import PaginationItem from "../PaginationItem";
-import {isUndefined} from "util";
 
 export const Pagination = ({
-                             children,
-                             previous,
-                             next,
-                             landmarkLabel,
-                             currentPageNumber,
-                             classes,
-                           }: PaginationProps) => {
+  children,
+  previous,
+  next,
+  landmarkLabel,
+  currentPageNumber,
+  classes,
+}: PaginationProps) => {
   const arrayChildren: any = Children.toArray(children);
+
+  const blockLevel = arrayChildren?.length < 1 && (previous || next);
 
   const finalArray: any = [];
   arrayChildren.forEach((child: any, index: number) => {
@@ -30,7 +31,7 @@ export const Pagination = ({
     else if (index === currentPageNumber-1) {
       finalArray.push(child);
     }
-    
+
     //next page
     else if (index === currentPageNumber) {
       finalArray.push(child);
@@ -52,84 +53,82 @@ export const Pagination = ({
     finalArray.splice((finalArray?.length - 1), 0, <PaginationItem ellipsis />)
   }
 
+  const nextArrow = (
+    <svg className="govuk-pagination__icon govuk-pagination__icon--next" xmlns="http://www.w3.org/2000/svg" height="13" width="15" aria-hidden="true" focusable="false" viewBox="0 0 15 13">
+      <path d="m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z"></path>
+    </svg>
+  );
+
   return (
-      <>
-        <nav className={`govuk-pagination ${classes ? classes : ''}`} role="navigation" aria-label={`${landmarkLabel ? landmarkLabel : 'results'}`}>
+    <>
+      <nav className={`govuk-pagination ${blockLevel ? 'govuk-pagination--block' : ''} ${classes ? classes : ''}`} role="navigation" aria-label={`${landmarkLabel ? landmarkLabel : 'results'}`}>
 
-          {previous && currentPageNumber > 1 && (
-            <>
-              <div className="govuk-pagination__prev">
-                <a className="govuk-link govuk-pagination__link" href={previous?.href} rel="prev">
-                  <svg
-                      className="govuk-pagination__icon govuk-pagination__icon--prev"
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="13"
-                      width="15"
-                      aria-hidden="true"
-                      focusable="false"
-                      viewBox="0 0 15 13"
-                  >
-                    <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path>
-                  </svg>
-                  <span className="govuk-pagination__link-title">
-                    {previous?.labelText ? previous?.labelText : 'Previous'}
-                  </span>
-                  {previous?.labelText && (
-                    <>
-                      <span className="govuk-visually-hidden">:</span>
-                      <span className="govuk-pagination__link-label">{previous.labelText}</span>
-                    </>
-                  )}
-                </a>
-              </div>
-            </>
-          )}
+        {previous && currentPageNumber > 1 && (
+          <>
+            <div className="govuk-pagination__prev">
+              <a className="govuk-link govuk-pagination__link" href={previous.href} rel="prev">
+                <svg
+                  className="govuk-pagination__icon govuk-pagination__icon--prev"
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="13"
+                  width="15"
+                  aria-hidden="true"
+                  focusable="false"
+                  viewBox="0 0 15 13"
+                >
+                  <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path>
+                </svg>
+                <span className={`govuk-pagination__link-title ${blockLevel && !previous.labelText ? 'govuk-pagination__link-title--decorated' : ''}`}>
+                  {previous.text ? previous.text : 'Previous'}
+                </span>
+                {previous.labelText && blockLevel && (
+                  <>
+                    <span className="govuk-visually-hidden">:</span>
+                    <span className="govuk-pagination__link-label">{previous.labelText}</span>
+                  </>
+                )}
+              </a>
+            </div>
+          </>
+        )}
 
-          { children && (
-            <ul className="govuk-pagination__list">
-              {finalArray?.length > 0 && Children.map(finalArray, (child: any, index) => {
-                if (isValidElement(child) && (child.type === PaginationItem)) {
-                  return (
-                    <>
-                      {
-                        cloneElement(child as React.ReactElement<any>, {
-                          current: currentPageNumber?.toString() === child.props.number
-                        })
-                      }
-                    </>
-                  );
-                }
-              })}
-            </ul>
-          )}
+        { children && (
+          <ul className="govuk-pagination__list">
+            {finalArray?.length > 0 && Children.map(finalArray, (child: any, index) => {
+              if (isValidElement(child) && (child.type === PaginationItem)) {
+                return (
+                  <>
+                    {
+                      cloneElement(child as React.ReactElement<any>, {
+                        current: currentPageNumber?.toString() === child.props.number
+                      })
+                    }
+                  </>
+                );
+              }
+            })}
+          </ul>
+        )}
 
-          {next && currentPageNumber !== arrayChildren.length && (
-              <div className="govuk-pagination__next">
-                <a className="govuk-link govuk-pagination__link" href={next.href} rel="next">
-                  <span className="govuk-pagination__link-title">{next.text ? next.text : 'Next'}</span>
-                  {next.labelText && (
-                      <>
-                        <span className="govuk-visually-hidden">:</span>
-                        <span className="govuk-pagination__link-label">{next.labelText}</span>
-                      </>
-                  )}
-                  <svg
-                      className="govuk-pagination__icon govuk-pagination__icon--next"
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="13"
-                      width="15"
-                      aria-hidden="true"
-                      focusable="false"
-                      viewBox="0 0 15 13"
-                  >
-                    <path
-                        d="m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z"></path>
-                  </svg>
-                </a>
-              </div>
-          )}
-        </nav>
-      </>
+        {next && currentPageNumber !== arrayChildren.length && (
+          <div className="govuk-pagination__next">
+            <a className="govuk-link govuk-pagination__link" href={next.href} rel="next">
+              { blockLevel && nextArrow }
+              <span className={`govuk-pagination__link-title ${blockLevel && !next.labelText ? 'govuk-pagination__link-title--decorated' : ''}`}>
+                {next.text ? next.text : 'Next'}
+              </span>
+              {next.labelText && blockLevel && (
+                <>
+                  <span className="govuk-visually-hidden">:</span>
+                  <span className="govuk-pagination__link-label">{next.labelText}</span>
+                </>
+              )}
+              { !blockLevel && nextArrow }
+            </a>
+          </div>
+        )}
+      </nav>
+    </>
   );
 };
 
--- src/stories/components/Pagination/Pagination.stories.tsx
+++ src/stories/components/Pagination/Pagination.stories.tsx
@@ -26,14 +26,106 @@ const primary: Story = {
       <PaginationItem href={"#"} number="1" />,
       <PaginationItem href={"#"} number="2" />,
       <PaginationItem href={"#"} number="3" />,
+    ]
+  }
+};
+
+const largeNumberOfPages: Story = {
+  name: "Large number of pages",
+  args: {
+    previous: {
+      href: "#"
+    },
+    next: {
+      href: "#"
+    },
+    currentPageNumber: 5,
+    children: [
+      <PaginationItem href={"#"} number="1" />,
+      <PaginationItem href={"#"} number="2" />,
+      <PaginationItem href={"#"} number="3" />,
       <PaginationItem href={"#"} number="4" />,
       <PaginationItem href={"#"} number="5" />,
       <PaginationItem href={"#"} number="6" />,
       <PaginationItem href={"#"} number="7" />,
+      <PaginationItem href={"#"} number="8" />,
+      <PaginationItem href={"#"} number="9" />,
+      <PaginationItem href={"#"} number="42" />,
+    ]
+  }
+};
+
+const numberLabels: Story = {
+  name: "Number labels",
+  args: {
+    previous: {
+      href: "#",
+      labelText: "1 of 3"
+    },
+    next: {
+      href: "#",
+      labelText: "3 of 3"
+    },
+    currentPageNumber: 2,
+  }
+};
+
+const linkLabels: Story = {
+  name: "Number labels",
+  args: {
+    previous: {
+      href: "#",
+      labelText: "Applying for a provisional lorry or bus licence",
+    },
+    next: {
+      href: "#",
+      labelText: "Driver CPC part 1 test: theory"
+    },
+    currentPageNumber: 2,
+  }
+};
+
+const firstPage: Story = {
+  name: "First page",
+  args: {
+    previous: {
+      href: "#"
+    },
+    next: {
+      href: "#"
+    },
+    currentPageNumber: 1,
+    children: [
+      <PaginationItem href={"#"} number="1" />,
+      <PaginationItem href={"#"} number="2" />,
+      <PaginationItem href={"#"} number="3" />,
+    ]
+  }
+};
+
+const lastPage: Story = {
+  name: "First page",
+  args: {
+    previous: {
+      href: "#"
+    },
+    next: {
+      href: "#"
+    },
+    currentPageNumber: 3,
+    children: [
+      <PaginationItem href={"#"} number="1" />,
+      <PaginationItem href={"#"} number="2" />,
+      <PaginationItem href={"#"} number="3" />,
     ]
   }
 };
 
 export {
   primary,
+  largeNumberOfPages,
+  numberLabels,
+  linkLabels,
+  firstPage,
+  lastPage,
 };
