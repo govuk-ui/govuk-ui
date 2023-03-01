c634b9673c1b41a40b836bc11a635139c220dd02
Nathan Maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2023-03-01 19:34:57 +0000

    Updates

---
+++ assets/Details-76e5a617.js
@@ -0,0 +1,19 @@
+import{a as t,j as p,F as c}from"./jsx-runtime-68f49b4e.js";import{M as h,C as r,S as s}from"./chunk-4XCFV5WA-a5ca2b84.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{primary as a}from"./Details.stories-80a883ce.js";import"./Accordion-12d9884c.js";import"./BackLink-54564ca1.js";import"./BreadcrumbItem-7e03e4ef.js";import"./Button-791dc627.js";import"./CharacterCount-6c285db3.js";import"./Table-e8f8e434.js";import"./CookieBanner-5f4494f2.js";import"./DateInput-4b08d3af.js";import"./Details-7a2689db.js";import"./ErrorMessage-07ea8fac.js";import"./ErrorSummary-d6d61f90.js";import"./FileUpload-b260dab3.js";import"./Footer-aedacc55.js";import"./Header-81057254.js";import{H as l}from"./Hint-47130fa7.js";import"./Input-70154cc0.js";import"./InsetText-39830db6.js";import"./Label-3d09487c.js";import"./NotificationBanner-b6ddf0d7.js";import"./Pagination-b98a4ff4.js";import"./Panel-9db3f9a0.js";import"./PhaseBanner-b7bf4a2c.js";import"./Radios-78e6f060.js";import"./Select-d6e3d713.js";import"./SkipLink-0b18f351.js";import"./Tabs-61739ac7.js";import"./Textarea-0f1928d4.js";import"./WarningText-7f7d45da.js";import{u as m}from"./index-9ec211f2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-1c0bb71e.js";import"../sb-preview/runtime.mjs";import"./index-2d4beb60.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-356e4a49.js";import"./FormGroup-f91b51d3.js";import"./Fieldset-cd643f71.js";import"./Legend-645732ce.js";import"./Typography-3950099a.js";function it(e={}){const{wrapper:n}=Object.assign({},m(),e.components);return n?t(n,Object.assign({},e,{children:t(i,{})})):i();function i(){const o=Object.assign({h1:"h1",p:"p",h2:"h2",h3:"h3"},m(),e.components);return p(c,{children:[t(h,{title:"Details"}),`
+`,t(l,{children:"Components"}),`
+`,t(o.h1,{children:"Details"}),`
+`,t(o.p,{children:"Make a page easier to scan by letting users reveal more detailed information only if they need it."}),`
+`,t(r,{children:t(s,{of:a})}),`
+`,t(o.h2,{children:"When to use this component"}),`
+`,t(o.p,{children:"Use the details component to make a page easier to scan when it contains information that only some users will need."}),`
+`,t(o.h2,{children:"When not to use this component"}),`
+`,t(o.p,{children:"Do not use the details component to hide information that the majority of your users will need."}),`
+`,t(o.h2,{children:"Decide between using details, accordions and tabs"}),`
+`,t(o.p,{children:"Details, accordions, and tabs all hide sections of content which a user can choose to reveal."}),`
+`,t(o.p,{children:"Use the details component instead of tabs or an accordion if you only have 1 section of content."}),`
+`,t(o.p,{children:"The details component is less visually prominent than tabs and accordions, so tends to work better for content which is not as important to users."}),`
+`,t(o.h2,{children:"How it works"}),`
+`,t(o.p,{children:"The details component is a short link that shows more detailed help text when a user clicks on it."}),`
+`,t(r,{children:t(s,{of:a})}),`
+`,t(o.h3,{children:"Write clear link text"}),`
+`,t(o.p,{children:"Make the link text short and descriptive so users can quickly work out if they need to click on it."})]})}}export{it as default};
+//# sourceMappingURL=Details-76e5a617.js.map
