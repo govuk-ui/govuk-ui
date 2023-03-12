a061fd361d5f830072f0663e78717588bf0bbcc4
Nathan Maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2023-03-12 20:17:08 +0000

    Re add type checking for Tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -8,13 +8,14 @@ export const Tabs = ({ children, classes, id, title, attributes }: TabsProps) =>
   const arrayChildren: any = Children.toArray(children);
 
   return (
-    <>
-      <div className={`govuk-tabs ${classes || ''}`} data-module="govuk-tabs" { ...attributes }>
+    <div className={`govuk-tabs ${classes || ''}`} data-module="govuk-tabs" { ...attributes }>
 
-        <h2 className="govuk-tabs__title">{title || "Contents"}</h2>
+      <h2 className="govuk-tabs__title">{title || "Contents"}</h2>
 
-        <ul className="govuk-tabs__list">
-          { Children.map(arrayChildren, (child:any, index) => {
+      <ul className="govuk-tabs__list">
+        { Children.map(arrayChildren, (child:any, index) => {
+          if (isValidElement(child) && (child.type === TabItem)) {
+            child = child as TabItemProps
             return (
               <>
                 {
@@ -24,18 +25,21 @@ export const Tabs = ({ children, classes, id, title, attributes }: TabsProps) =>
                 }
               </>
             );
-          })}
-        </ul>
+          }
+        })}
+      </ul>
 
-        { Children.map(arrayChildren, (child:any, index) => {
+      { Children.map(arrayChildren, (child:any, index) => {
+        if (isValidElement(child) && (child.type === TabItem)) {
+          child = child as TabItemProps
           return (
             <div className={`govuk-tabs__panel ${child?.props.selected ? '' : 'govuk-tabs__panel--hidden'}`} id={child.props.id ? child.props.id : `tab-item-${index + 1}`}>
               { child?.props.children }
             </div>
           );
-        })}
-      </div>
-    </>
+        }
+      })}
+    </div>
   );
 };
 
