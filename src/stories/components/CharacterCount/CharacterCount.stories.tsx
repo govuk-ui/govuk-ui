f11e5cd11a8c76ce2d43391249fd98a6ee0b4d25
Craig Taylor <craig.taylor3@engineering.digital.dwp.gov.uk>
2023-03-28 15:47:49 +0100

    character count docs

--- src/stories/components/CharacterCount/CharacterCount.stories.tsx
+++ src/stories/components/CharacterCount/CharacterCount.stories.tsx
@@ -33,6 +33,85 @@ const primary: Story = {
   )
 }
 
+const multipleQuestions: Story = {
+  name: 'Multiple questions character count',
+  render: () => (
+    <>
+      <CharacterCount
+        id="example-textarea"
+        maxLength={200}
+      >
+        <Label>
+          <Typography component="h1" variant="body">
+            Describe the nature of your event
+          </Typography>
+        </Label>
+      </CharacterCount>
+    </>
+  )
+}
+
+const oneFiftyWords: Story = {
+  name: 'One fifty words character count',
+  render: () => (
+    <>
+      <CharacterCount
+        id="example-textarea"
+        maxWords={150}
+      >
+        <Label>
+          <Typography component="h1" variant="l">
+            Enter a job description
+          </Typography>
+        </Label>
+      </CharacterCount>
+    </>
+  )
+}
+
+const threshold: Story = {
+  name: 'Threshold character count',
+  render: () => (
+    <>
+      <CharacterCount
+        id="example-textarea"
+        maxLength={112}
+        threshold={75}
+      >
+        <Label>
+          <Typography component="h1" variant="l">
+            Can you provide more detail?
+          </Typography>
+        </Label>
+        Type another letter into this field after this message to see the threshold feature
+      </CharacterCount>
+    </>
+  )
+}
+
+const errorInPlace: Story = {
+  name: 'Error in place character count',
+  render: () => (
+    <>
+      <CharacterCount
+        id="example-textarea"
+        maxWords={150}
+        errorMessage="Job description must be 350 characters or less"
+      >
+        <Label>
+          <Typography component="h1" variant="l">
+            Enter a job description
+          </Typography>
+        </Label>
+      </CharacterCount>
+    </>
+  )
+}
+
 export {
   primary,
+  multipleQuestions,
+  oneFiftyWords,
+  threshold,
+  errorInPlace,
 }
