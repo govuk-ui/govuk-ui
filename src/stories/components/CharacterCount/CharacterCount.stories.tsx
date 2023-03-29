dbfd1857e1675531bb884c34959fb5f1b46bf2c6
Craig Taylor <craig.taylor3@engineering.digital.dwp.gov.uk>
2023-03-29 13:45:35 +0100

    fix no component code

--- src/stories/components/CharacterCount/CharacterCount.stories.tsx
+++ src/stories/components/CharacterCount/CharacterCount.stories.tsx
@@ -14,99 +14,88 @@ type Story = StoryObj<typeof CharacterCount>;
 
 const primary: Story = {
   name: 'Primary character count',
-  render: () => (
-    <>
-      <CharacterCount
-        id="example-textarea"
-        maxLength={200}
-      >
-        <Label>
-          <Typography component="h1" variant="l">
-            Can you provide more detail?
-          </Typography>
-        </Label>
-        <Hint>
-        Do not include personal or financial information like your National Insurance number or credit card details.
-        </Hint>
-      </CharacterCount>
-    </>
+  render: (args) => (
+    <CharacterCount
+      id="example-textarea"
+      maxLength={200}
+    >
+      <Label>
+        <Typography component="h1" variant="l">
+          Can you provide more detail?
+        </Typography>
+      </Label>
+      <Hint>
+      Do not include personal or financial information like your National Insurance number or credit card details.
+      </Hint>
+    </CharacterCount>
   )
 }
 
 const multipleQuestions: Story = {
   name: 'Multiple questions character count',
-  render: () => (
-    <>
-      <CharacterCount
-        id="example-textarea"
-        maxLength={200}
-      >
-        <Label>
-          <Typography component="h1" variant="body">
-            Describe the nature of your event
-          </Typography>
-        </Label>
-      </CharacterCount>
-    </>
+  render: (args) => (
+    <CharacterCount
+      id="example-textarea"
+      maxLength={200}
+    >
+      <Label>
+        <Typography component="h1" variant="body">
+          Describe the nature of your event
+        </Typography>
+      </Label>
+    </CharacterCount>
   )
 }
 
 const oneFiftyWords: Story = {
   name: 'One fifty words character count',
-  render: () => (
-    <>
-      <CharacterCount
-        id="example-textarea"
-        maxWords={150}
-      >
-        <Label>
-          <Typography component="h1" variant="l">
-            Enter a job description
-          </Typography>
-        </Label>
-      </CharacterCount>
-    </>
+  render: (args) => (
+    <CharacterCount
+      id="example-textarea"
+      maxWords={150}
+    >
+      <Label>
+        <Typography component="h1" variant="l">
+          Enter a job description
+        </Typography>
+      </Label>
+    </CharacterCount>
   )
 }
 
 const threshold: Story = {
   name: 'Threshold character count',
-  render: () => (
-    <>
-      <CharacterCount
-        id="example-textarea"
-        maxLength={112}
-        threshold={75}
-        value="Type another letter into this field after this message to see the threshold feature"
-      >
-        <Label>
-          <Typography component="h1" variant="l">
-            Can you provide more detail?
-          </Typography>
-        </Label>
-        
-      </CharacterCount>
-    </>
+  render: (args) => (
+    <CharacterCount
+      id="example-textarea"
+      maxLength={112}
+      threshold={75}
+      value="Type another letter into this field after this message to see the threshold feature"
+    >
+      <Label>
+        <Typography component="h1" variant="l">
+          Can you provide more detail?
+        </Typography>
+      </Label>
+    </CharacterCount>
   )
 }
 
 const errorInPlace: Story = {
   name: 'Error in place character count',
-  render: () => (
-    <>
-      <CharacterCount
-        id="example-textarea"
-        maxWords={150}
-        errorMessage="Job description must be 350 characters or less"
-        value="A content designer works on the end-to-end journey of a service to help users complete their goal and government deliver a policy intent. Their work may involve the creation of, or change to, a transaction, product or single piece of content that stretches across digital and offline channels. They make sure appropriate content is shown to a user in the right place and in the best format."
-      >
-        <Label>
-          <Typography component="h1" variant="l">
-            Enter a job description
-          </Typography>
-        </Label>
-      </CharacterCount>
-    </>
+  render: (args) => (
+    <CharacterCount
+      id="example-textarea"
+      maxWords={150}
+      errorMessage="Job description must be 350 characters or less"
+      value="A content designer works on the end-to-end journey of a service to help users complete their goal and government deliver a policy intent. Their work may involve the creation of, or change to, a transaction, product or single piece of content that stretches across digital and offline channels. They make sure appropriate content is shown to a user in the right place and in the best format."
+    >
+      <Label>
+        <Typography component="h1" variant="l">
+          Enter a job description
+        </Typography>
+      </Label>
+    </CharacterCount>
   )
 }
 
