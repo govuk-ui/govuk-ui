c0ae85365c4bb5baa6931dd360cc6f949bbb844e
Craig Taylor <craig.taylor3@engineering.digital.dwp.gov.uk>
2023-03-31 10:58:05 +0100

    stories

---
+++ src/stories/components/InsetText/InsetText.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import InsetText from "../../../components/InsetText";
+
+const meta: Meta<typeof InsetText> = {
+  title: "InsetText",
+  component: InsetText,
+};
+
+export default meta;
+type Story = StoryObj<typeof InsetText>;
+
+const primary: Story = {
+  name: 'Inset Text',
+  args: {
+    children: 'It can take up to 8 weeks to register a lasting power of attorney if there are no mistakes in the application.'
+  }
+}
+
+export {
+  primary,
+};
