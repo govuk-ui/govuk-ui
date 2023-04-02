7ca908e8825ba6840bf50a7a6808cf70565ed032
nathan.maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2023-04-02 22:09:46 +0100

    Updates

---
+++ assets/Footer-624a29cc.js
@@ -0,0 +1,33 @@
+import{j as e,a as r,F as l}from"./jsx-runtime-670450c2.js";import{M as p,C as n,S as t}from"./index-d1463bd2.js";import"./chunk-PCJTTTQV-2cdeb1b5.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{primary as c,withLinks as m,secondaryNavigation as d,linksAndSecondaryNavigation as u}from"./Footer.stories-f9a15bf9.js";import"./Accordion-b094a9f4.js";import"./BackLink-5d9697e5.js";import"./BreadcrumbItem-5d1e14c2.js";import"./Button-232179fb.js";import"./CharacterCount-9424c10a.js";import"./CookieBanner-0021e8bc.js";import"./Details-ea8c166a.js";import"./ErrorMessage-5eec86f5.js";import"./FileUpload-a04d48dc.js";import"./Footer-f36a6c81.js";import"./Header-357af313.js";import{H as f}from"./Hint-44397457.js";import"./Radios-3ebc561f.js";import"./InsetText-5be58372.js";import"./Label-8f102d27.js";import"./NotificationBanner-9be16c78.js";import"./Pagination-1429ab7b.js";import"./PhaseBanner-809e0913.js";import"./Select-0fca9528.js";import"./SkipLink-6f274826.js";import"./Tabs-8973fdcc.js";import"./Textarea-d525196e.js";import"./WarningText-53f07e98.js";import{u as h}from"./index-9ec211f2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-c13b6213.js";import"../sb-preview/runtime.mjs";import"./index-d475d2ea.js";import"./index-f64090b0.js";import"./index-d37d4223.js";import"./index-356e4a49.js";import"./react-18-61705542.js";import"./FormGroup-b495daf4.js";import"./Fieldset-2e55f4ed.js";import"./Container-c2a0e93b.js";import"./Grid-c05eecf9.js";import"./Typography-a2bc044e.js";function re(i={}){const{wrapper:a}=Object.assign({},h(),i.components);return a?e(a,Object.assign({},i,{children:e(s,{})})):s();function s(){const o=Object.assign({h1:"h1",p:"p",h2:"h2",a:"a",ul:"ul",li:"li"},h(),i.components);return r(l,{children:[e(p,{title:"Components/Footer"}),`
+`,e(f,{children:"Components"}),`
+`,e(o.h1,{children:"Footer"}),`
+`,e(o.p,{children:"The footer provides copyright, licensing and other information about your service."}),`
+`,e(n,{children:e(t,{of:c})}),`
+`,e(o.p,{children:"If you use the page template, you’ll also get the footer without having to add it, as it’s included by default. However, if you want to customise the default footer, read the page template guidance about customising components."}),`
+`,e(o.h2,{children:"When to use this component"}),`
+`,e(o.p,{children:"Use the footer at the bottom of every page of your service."}),`
+`,e(o.h2,{children:"How it works"}),`
+`,e(o.p,{children:"Add a copyright notice to the footer to clarify who owns the copyright. For GOV.UK services, add the coat of arms to keep things consistent with the rest of GOV.UK."}),`
+`,r(o.p,{children:["Make it clear whether content is available for re-use - and if it is, under what sort of licence. Use an ",e(o.a,{href:"https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/",children:"Open Government Licence"})," unless you have permission from the National Archives to use a ",e(o.a,{href:"https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/open-government-licence/other-licences/",children:"different type of licence"}),"."]}),`
+`,e(o.h2,{children:"Footer without links"}),`
+`,e(n,{children:e(t,{of:c})}),`
+`,e(o.h2,{children:"Footer with links"}),`
+`,e(n,{children:e(t,{of:m})}),`
+`,e(o.h2,{children:"Adding links"}),`
+`,e(o.p,{children:"You can add links to:"}),`
+`,r(o.ul,{children:[`
+`,e(o.li,{children:"privacy notice"}),`
+`,e(o.li,{children:"accessibility statement"}),`
+`,e(o.li,{children:"cookies page"}),`
+`,e(o.li,{children:"terms and conditions"}),`
+`,e(o.li,{children:"other language options"}),`
+`]}),`
+`,e(o.p,{children:"Use ‘Privacy’, ‘Accessibility’, ‘Cookies’ and ‘Terms and conditions’ for the link text."}),`
+`,e(o.h2,{children:"Adding secondary navigation"}),`
+`,e(o.p,{children:`Only add secondary GOV.UK navigation if you’re creating a GOV.UK service, and you want users to be able to navigate away from the service to somewhere else on the GOV.UK website.
+For example, you probably don’t want to encourage a user to navigate away from a linear, form-type service.`}),`
+`,e(o.h2,{children:"Footer with secondary navigation"}),`
+`,e(n,{children:e(t,{of:d})}),`
+`,e(o.h2,{children:"Footer with links and secondary navigation"}),`
+`,e(n,{children:e(t,{of:u})})]})}}export{re as default};
+//# sourceMappingURL=Footer-624a29cc.js.map
