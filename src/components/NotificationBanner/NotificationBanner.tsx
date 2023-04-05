3eb4198cf68490caca0488f96a3c6ff2c6c193d7
Craig Taylor <craig.taylor3@engineering.digital.dwp.gov.uk>
2023-04-05 12:09:46 +0100

    notification banner

--- src/components/NotificationBanner/NotificationBanner.tsx
+++ src/components/NotificationBanner/NotificationBanner.tsx
@@ -2,33 +2,35 @@ import React from "react";
 import NotificationBannerProps from "./NotificationBanner.types";
 
 export const NotificationBanner = ({
-  text,
-  html,
+  title,
+  children,
   type,
-  disableAutoFocus,
   role,
-  attributes,
-  titleText,
-  titleHtml,
+  disableAutoFocus,
   titleHeadingLevel,
-  titleId,
   classes,
+  ...attributes
 }: NotificationBannerProps) => {
+
+  if (!role) {
+    role = 'region;'
+  }
   return (
     <>
       <div
-        className="govuk-notification-banner"
-        role="region"
+        className={`govuk-notification-banner ${type === 'success' ? 'govuk-notification-banner--success' : ''} ${classes || ''}`}
+        role={role}
         aria-labelledby="govuk-notification-banner-title"
         data-module="govuk-notification-banner"
+        { ...attributes }
       >
         <div className="govuk-notification-banner__header">
           <h2 className="govuk-notification-banner__title" id="govuk-notification-banner-title">
-            Important
+            {title}
           </h2>
         </div>
         <div className="govuk-notification-banner__content">
-          <p className="govuk-notification-banner__heading">This publication was withdrawn on 7 March 2014.</p>
+          {children}
         </div>
       </div>
     </>
