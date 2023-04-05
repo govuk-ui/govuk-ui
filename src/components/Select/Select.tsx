9f5ac29bdf1717a7a1acc498a9c6d2acba76e199
nathan.maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2023-04-05 18:01:58 +0100

    Select component and docs

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, {Children, cloneElement, isValidElement} from "react";
 import SelectProps from "./Select.types";
 import FormGroup from "../../layout/FormGroup";
 import ErrorMessage from "../ErrorMessage";
+import Hint from "../Hint";
+import Label from "../Label";
+import SelectItem from "../SelectItem";
 
 export const Select = ({
   id,
   name,
-  classes,
-  children,
-  errorMessage,
+  value,
   describedBy,
+  errorMessage,
+  children,
+  classes,
+  formGroupClasses,
   ...attributes
 }: SelectProps) => {
   let describedByValue = describedBy || '';
@@ -27,11 +32,42 @@ export const Select = ({
     name = id;
   }
 
+  const arrayChildren = Children.toArray(children);
+
+  { Children.map(arrayChildren, (child:any, _index) => {
+    if (isValidElement(child) && (child.type === Hint)) {
+      describedByValue += ` ${id}-hint`;
+    }
+  })}
+
   return (
-    <FormGroup error={errorMessage}>
+    <FormGroup error={errorMessage} classes={formGroupClasses}>
+      { Children.map(arrayChildren, (child:any, _index) => {
+        if (isValidElement(child) && ((child.type === Label) || (child.type === Hint))) {
+          return (
+              <>
+                {
+                  cloneElement(child as React.ReactElement<any>, {})
+                }
+              </>
+          );
+        }
+      })}
       {errorMessageComponent}
       <select className={`govuk-select ${classes || ''}`} id={id} name={name} aria-describedby={describedByValue} {...attributes}>
-        { children }
+        { Children.map(arrayChildren, (child:any, _index) => {
+          if (isValidElement(child) && (child.type === SelectItem)) {
+            return (
+              <>
+                {
+                  cloneElement(child as React.ReactElement<any>, {
+                    data: value
+                  })
+                }
+              </>
+            );
+          }
+        })}
       </select>
     </FormGroup>
   );
