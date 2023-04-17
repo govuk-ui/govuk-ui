cf5d1e060c3c6b5ddb6efee7c228868c57dc9b26
Craig Taylor <craig.taylor3@engineering.digital.dwp.gov.uk>
2023-04-17 15:15:07 +0100

    accordion

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,62 +1,42 @@
-import React from "react";
+import React, {Children, cloneElement, isValidElement} from "react";
+import AccordionItem from "../AccordionItem/AccordionItem";
 import AccordionProps from "./Accordion.types";
 
 export const Accordion = ({
   id,
-  items,
-  hideAllSectionsText,
-  showAllSectionsText,
-  hideSectionText,
-  hideSectionAriaLabelText,
-  showSectionText,
-  showSectionAriaLabelText,
+  name,
+  children,
   classes,
-  attributes,
   headingLevel,
+  ...attributes
 }: AccordionProps) => {
-  return (
-    <>
-      <div className="govuk-accordion" data-module="govuk-accordion" id="default-example">
-        <div className="govuk-accordion__section ">
-          <div className="govuk-accordion__section-header">
-            <h2 className="govuk-accordion__section-heading">
-              <span className="govuk-accordion__section-button" id="default-example-heading-1">
-                Section A
-              </span>
-            </h2>
-          </div>
-          <div
-            id="default-example-content-1"
-            className="govuk-accordion__section-content"
-            aria-labelledby="default-example-heading-1"
-          >
-            <p className="govuk-body">
-              We need to know your nationality so we can work out which elections you’re entitled to vote in. If you
-              cannot provide your nationality, you’ll have to send copies of identity documents through the post.
-            </p>
-          </div>
-        </div>
 
-        <div className="govuk-accordion__section ">
-          <div className="govuk-accordion__section-header">
-            <h2 className="govuk-accordion__section-heading">
-              <span className="govuk-accordion__section-button" id="default-example-heading-2">
-                Section B
-              </span>
-            </h2>
-          </div>
-          <div
-            id="default-example-content-2"
-            className="govuk-accordion__section-content"
-            aria-labelledby="default-example-heading-2"
-          >
-            <ul className="govuk-list govuk-list--bullet">
-              <li>Example item 2</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
+  if (!id && name) {
+    id = name;
+  } else if (!name && id) {
+    name = id;
+  }
+
+  const arrayChildren: any = Children.toArray(children);
+
+  return (
+    <div className={`govuk-accordion${classes ? ` ${classes}` : ''}`} data-module="govuk-accordion" id={id} {...attributes}>
+      { Children.map(arrayChildren, (child:any, index) => {
+        if (isValidElement(child) && (child.type === AccordionItem)) {
+          return (
+            <>
+              {
+                cloneElement(child as React.ReactElement<any>, {
+                  id,
+                  index,
+                  headingLevel,
+                })
+              }
+            </>
+          );
+        }
+      })}
+    </div>
   );
 };
 
--- src/stories/components/Accordion/Accordion.stories.tsx
+++ src/stories/components/Accordion/Accordion.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import Accordion from "../../../components/Accordion";
 import React from "react";
+import AccordionItem from "../../../components/AccordionItem/AccordionItem";
+import { Typography } from "../../..";
 
 const meta: Meta<typeof Accordion> = {
   title: "Accordion",
@@ -12,9 +14,23 @@ export default meta;
 type Story = StoryObj<typeof Accordion>;
 
 const primary: Story = {
-  name: 'Accordion',
-  args: {
-  }
+  name: 'default',
+  render: (args) => (
+    <Accordion id={'example-accordion'}>
+      <AccordionItem heading="Writing well for the web" expanded>
+        <Typography>This is the content for Writing well for the web.</Typography>
+      </AccordionItem>
+      <AccordionItem heading="Writing well for specialists">
+        <Typography>This is the content for Writing well for specialists.</Typography>
+      </AccordionItem>
+      <AccordionItem heading="Know your audience">
+        <Typography>This is the content for Know your audience.</Typography>
+      </AccordionItem>
+      <AccordionItem heading="How people read">
+        <Typography>This is the content for How people read.</Typography>
+      </AccordionItem>
+    </Accordion>
+)
 }
 
 export {
