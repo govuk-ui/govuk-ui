6f2683496f10d00e963bff36726fe0603017a8bf
nathan.maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2023-04-26 10:01:21 +0100

    Updates

---
+++ assets/Breadcrumbs-fa4bc29c.js
@@ -0,0 +1,19 @@
+import{j as e,a as n,F as p}from"./jsx-runtime-670450c2.js";import{M as h,C as r}from"./index-c74fa780.js";import"./chunk-PCJTTTQV-9a82ccd3.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{primary as a,collapseOnMobile as c}from"./Breadcrumbs.stories-49fe86ea.js";import"./Accordion-5a2ba4cd.js";import"./BackLink-5d9697e5.js";import"./BreadcrumbItem-5d1e14c2.js";import"./Button-e68229e2.js";import"./CookieBanner-791a857d.js";import"./Details-ea8c166a.js";import{H as d}from"./Radios-0a1531ac.js";import"./Footer-d07f7a45.js";import"./Header-357af313.js";import"./InsetText-5be58372.js";import"./Pagination-1429ab7b.js";import"./PhaseBanner-e4ef22fe.js";import"./SkipLink-6f274826.js";import"./WarningText-53f07e98.js";import{u as m}from"./index-9ec211f2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-fc09acd1.js";import"../sb-preview/runtime.mjs";import"./index-d475d2ea.js";import"./index-f64090b0.js";import"./index-d37d4223.js";import"./index-356e4a49.js";import"./react-18-61705542.js";import"./FormGroup-b495daf4.js";import"./Fieldset-2e55f4ed.js";import"./Container-c2a0e93b.js";import"./Grid-c05eecf9.js";import"./Typography-a2bc044e.js";function K(t={}){const{wrapper:i}=Object.assign({},m(),t.components);return i?e(i,Object.assign({},t,{children:e(s,{})})):s();function s(){const o=Object.assign({h1:"h1",p:"p",h2:"h2",code:"code"},m(),t.components);return n(p,{children:[e(h,{title:"Components/Breadcrumbs"}),`
+`,e(d,{children:"Components"}),`
+`,e(o.h1,{id:"breadcrumbs",children:"Breadcrumbs"}),`
+`,e(o.p,{children:"The breadcrumbs component helps users to understand where they are within a website’s structure and move between levels."}),`
+`,e(r,{of:a}),`
+`,e(o.h2,{id:"when-to-use-this-component",children:"When to use this component"}),`
+`,e(o.p,{children:"Use the breadcrumbs component when you need to help users understand and move between the multiple levels of a website."}),`
+`,e(o.h2,{id:"when-not-to-use-this-component",children:"When not to use this component"}),`
+`,e(o.p,{children:"Do not use the breadcrumbs component on websites with a flat structure, or to show progress through a linear journey or transaction."}),`
+`,e(o.p,{children:"If you’re using other navigational elements on the page, such as a sidebar, consider whether your users need the additional support of breadcrumbs."}),`
+`,e(o.h2,{id:"how-it-works",children:"How it works"}),`
+`,n(o.p,{children:["Always place breadcrumbs at the top of a page, before the ",e(o.code,{children:"<main>"})," element. Placing them here means that the ‘Skip to main content’ link allows the user to skip all navigation links, including breadcrumbs."]}),`
+`,e(o.p,{children:"The breadcrumb should start with your ‘home’ page and end with the parent section of the current page."}),`
+`,e(r,{of:a}),`
+`,e(o.h2,{id:"collapsing-breadcrumbs-on-mobile-devices",children:"Collapsing breadcrumbs on mobile devices"}),`
+`,e(o.p,{children:"If you have long breadcrumbs you can configure the component to only show the first and last items on mobile devices."}),`
+`,n(o.p,{children:["To do this, add the prop ",e(o.code,{children:"collapseOnMobile"})," to the ",e(o.code,{children:"Radios"})," component as shown in this example."]}),`
+`,e(r,{of:c})]})}}export{K as default};
+//# sourceMappingURL=Breadcrumbs-fa4bc29c.js.map
